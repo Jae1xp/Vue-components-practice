dc96bb0158f8a1ad119be98a3a41b693212214d7
Jae Son <>
2018-12-11 15:02:49 -0500

    practicing refs

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-Vue.component('greeting', {
-  template: '<p>Hey there, I am a re-usable component</p>',
-  data: function(){
-    return {
-      name: 'Jae'
-    }
-  }
-});
+// Vue.component('greeting', {
+//   template: '<p>Hey there, I am a {{name}}. <button v-on:click="changeName">Change name</button></p>',
+//   data: function(){
+//     return {
+//       name: 'Jae'
+//     }
+//   },
+//   methods: {
+//     changeName: function(){
+//       this.name = 'BMO';
+//     }
+//   }
+// });
+
+
+// REFS
+// new Vue ({
+//   el: '#app',
+//   data: {
+//     output: ''
+//   },
+//   methods: {
+//     readRefs: function() {
+//       console.log(this.$refs.input.value);
+//       this.output = this.$refs.input.value;
+//     }
+//   }
+// });
+// End Refs
